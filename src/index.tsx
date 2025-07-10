19eccd8551f60c8f7af46f1b7ffc9318e249dcb6
CPunisher <1343316114@qq.com>
2025-07-10 19:02:31 +0800

    Remove worker

--- src/index.tsx
+++ src/index.tsx
@@ -1,1 +1,42 @@
-const worker = new Worker(new URL("./worker.js", import.meta.url));
+import { rspack, builtinMemFs } from "@rspack/browser";
+// import { BasicReact } from "./project";
+// declare const NODE_MODULES: Record<string, string>;
+
+builtinMemFs.volume.fromJSON({
+  // ...NODE_MODULES,
+  // ...BasicReact,
+  "/index.js": "1",
+  //   "/index.js": `
+  // import a from "./a.js"
+  // console.log(a)`,
+  //   "/a.js": `export default 2`,
+  //   "/loader.js": `
+  //   function loader(content) {
+  //     return "export default 1"
+  //   }
+  //   module.exports = loader;`,
+});
+
+rspack(
+  {
+    entry: "./index.js",
+    module: {
+      rules: [
+        {
+          test: /a\.js$/,
+          loader: "./loader.js",
+        },
+      ],
+    },
+  },
+  (err, stats) => {
+    if (err) {
+      console.error(err);
+    }
+    console.log("Stats err: ", !!stats?.hasErrors());
+    console.log("Stats warn: ", !!stats?.hasWarnings());
+    console.log(stats?.toJson());
+
+    console.log(builtinMemFs.volume.toJSON());
+  }
+);
