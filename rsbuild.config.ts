cbc1686d2e3ca2459eef235d5c0ac383626fdd04
CPunisher <1343316114@qq.com>
2025-08-19 18:00:15 +0800

    Update

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -1,18 +1,10 @@
 import { defineConfig } from "@rsbuild/core";
 import { pluginReact } from "@rsbuild/plugin-react";
-import { serializeDirectoryToMemFS } from "./memfs";
-
-const NODE_MODULES = {
-  ...serializeDirectoryToMemFS("./node_modules/react"),
-  ...serializeDirectoryToMemFS("./node_modules/react-dom"),
-  ...serializeDirectoryToMemFS("./node_modules/scheduler"),
-};
 
 export default defineConfig({
   plugins: [pluginReact()],
   source: {
     define: {
-      NODE_MODULES: JSON.stringify(NODE_MODULES),
       "process.env.NODE_DEBUG_NATIVE": undefined,
     },
   },
--- src/index.tsx
+++ src/index.tsx
@@ -1,57 +1,23 @@
 import { rspack, builtinMemFs } from "@rspack/browser";
-// import { BasicReact } from "./project";
-// declare const NODE_MODULES: Record<string, string>;
+import { files, config } from "./projects/BasicReact";
 
 builtinMemFs.volume.fromJSON({
-  // ...NODE_MODULES,
-  // ...BasicReact,
-  // "/index.js": "1",
-  "/index.js": `
-  import a from "./a.js"
-  import b from "./b.js"
-  console.log(a)
-  console.log(b)`,
-  "/a.js": `export default 2`,
-  "/b.js": `export default 3`,
-  "/loader.js": `export default function loader(content) { return "export default \\"111\\"" }`,
-  // "/loader2.js": `module.exports = function loader(content) { return "export default \\"222\\"" }`,
-  "/template.js":
-    "module.exports = ({ htmlRspackPlugin }) => `<html><head><title>Hello</title></head><body></body></html>`",
+  ...files,
 });
 
 function run() {
-  rspack(
-    {
-      mode: "development",
-      devtool: false,
-      entry: "./index.js",
-      module: {
-        rules: [
-          {
-            test: /a\.js$/,
-            loader: "./loader.js",
-          },
-          {
-            test: /b\.js$/,
-            loader: "./loader.js",
-          },
-        ],
-      },
-      // plugins: [new rspack.HtmlRspackPlugin({ template: "./template.js" })],
-    },
-    (err, stats) => {
-      if (err) {
-        console.error(err);
-      }
-      console.log("Stats err: ", !!stats?.hasErrors());
-      console.log("Stats warn: ", !!stats?.hasWarnings());
-      console.log(stats?.toString({ all: false, errors: true }));
-
-      const json = builtinMemFs.volume.toJSON();
-      console.log(json);
-      console.log(json["/dist/main.js"] as any);
+  rspack(config, (err, stats) => {
+    if (err) {
+      console.error(err);
     }
-  );
+    console.log("Stats err: ", !!stats?.hasErrors());
+    console.log("Stats warn: ", !!stats?.hasWarnings());
+    console.log(stats?.toString({ all: false, errors: true }));
+
+    const json = builtinMemFs.volume.toJSON();
+    console.log(json);
+    console.log(json["/dist/main.js"] as any);
+  });
 }
 
 run();
