0bfa7ae18b676a75b97bd0f0d5558eb6684c6b54
CPunisher <1343316114@qq.com>
2025-11-26 16:46:13 +0800

    Inject react refresh

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -1,8 +1,30 @@
 import { defineConfig } from "@rsbuild/core";
 import { pluginReact } from "@rsbuild/plugin-react";
+import fs from "node:fs";
+
+const REACT_REFRESH_RUNTIME = fs.readFileSync(
+  "node_modules/react-refresh/cjs/react-refresh-runtime.development.js"
+);
+const RSPACK_REACT_REFRESH = fs.readFileSync(
+  "node_modules/@rspack/plugin-react-refresh/client/reactRefresh.js"
+);
+const RSPACK_REACT_REFRESH_ENTRY = fs.readFileSync(
+  "node_modules/@rspack/plugin-react-refresh/client/reactRefreshEntry.js"
+);
+const RSPACK_REACT_REFRESH_UTILS = fs.readFileSync(
+  "node_modules/@rspack/plugin-react-refresh/client/refreshUtils.js"
+);
 
 export default defineConfig({
   plugins: [pluginReact()],
+  source: {
+    define: {
+      REACT_REFRESH_RUNTIME: JSON.stringify(REACT_REFRESH_RUNTIME),
+      RSPACK_REACT_REFRESH: JSON.stringify(RSPACK_REACT_REFRESH),
+      RSPACK_REACT_REFRESH_ENTRY: JSON.stringify(RSPACK_REACT_REFRESH_ENTRY),
+      RSPACK_REACT_REFRESH_UTILS: JSON.stringify(RSPACK_REACT_REFRESH_UTILS),
+    },
+  },
   output: {
     minify: false,
   },
--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,6 @@
 /// <reference types="@rsbuild/core/types" />
+
+declare const REACT_REFRESH_RUNTIME: string;
+declare const RSPACK_REACT_REFRESH: string;
+declare const RSPACK_REACT_REFRESH_ENTRY: string;
+declare const RSPACK_REACT_REFRESH_UTILS: string;
